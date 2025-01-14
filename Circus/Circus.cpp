b8f3696df99bcc3487c51a47b4a2b6a36001a12f
Kacper Sikorski <115071106+Sikor915@users.noreply.github.com>
2025-01-14 21:06:59 +0100

    Revert "Updated asm version of algorithm to load data from OG image and store data in processed image. Also added version comments in both versions of the algorithm"
    This reverts commit 371422d64a7f9a46aed43b9afae41b1052f0ee6b.

--- Circus/Circus.cpp
+++ Circus/Circus.cpp
@@ -1,64 +1,54 @@
 #include "pch.h"
 #include "Circus.h"
 
-/*
-* Temat: Filtr dolnoprzepustowy kolowy
-* Algorytm usrednia wartosci pikseli bazujac na macierzy sasiadow 5x5, tworzac efekt rozmycia.
-* Ta implementacja pomija piksele na brzegach obrazu.
-* Data wykonania: 2025-01-14
-* Autor: Kacper Sikorski, INF/5 semestr
-* ---------------------------------------------------------------------------------------------------------------
-* Wersja 1.0 - 2024-11-03                                                                                       |
-* Dzialajacy algorytm w wersji C++. Wykorzystuje potrojna petle do iteracji po kazdym sasiedzie kazdego piksela.|
-* Algorytm przechodzi przez caly obraz, odpowiednio unikajac czesci poza obrazem.                               |
-* ---------------------------------------------------------------------------------------------------------------            
-* Wersja 1.1 - 2025-01-04                                                                                       |
-* Zmienilem dzialanie algorytmu by przechodzil on na bazie ilosci wierszy do przetworzenia, a nie na podstawie  |
-* wielkosci vectora.                                                                                            |
-* ---------------------------------------------------------------------------------------------------------------
-*/
-
-void CompressionFuncCircus(int* pixelData, int rowsToProcess, int width, const int* endPixelAddress, int* OGData) 
+void CompressionFuncCircus(int* pixelData, int rowsToProcess, int width, const int* endPixelAddress) 
 {
-    // Precomputed offsets for the 5x5 neighborhood excluding corners
-    const int offsets[21][2] = {
-                  {-2, -1}, {-2, 0}, {-2, 1},
-        {-1, -2}, {-1, -1}, {-1, 0}, {-1, 1}, {-1, 2},
-        {0, -2},  {0, -1} , {0, 0} , {0, 1} , {0, 2},
-        {1, -2},  {1, -1} , {1, 0} , {1, 1} , {1, 2},
-                  {2, -1} , {2, 0} , {2, 1}
-    };
     for (int row = 0; row < rowsToProcess; ++row) 
     {
         for (int col = 2; col < width - 2; ++col) 
         {
             // Calculate the current pixel's address
-            int* OGPixelAddress{ OGData + ((row)*width + (col - 2)) },
-                * updatedPixelAddress{ pixelData + ((row)*width + (col - 2)) };
+            int* currentPixelAddress = pixelData + ((row) * width + (col-2));
 
-            if (OGPixelAddress > endPixelAddress)
+            if (currentPixelAddress > endPixelAddress)
                 return;
 
-            int redSum{ 0 }, greenSum{ 0 }, blueSum{ 0 }, neighborPixel{ 0 },
-                avgRed{ 0 }, avgGreen{ 0 }, avgBlue{ 0 };
+            int redSum = 0, greenSum = 0, blueSum = 0;
 
-            for (int i = 0; i < 21; i++)
+            // Iterate over the 5x5 neighborhood
+            for (int y = -2; y <= 2; ++y) 
             {
-                int y = offsets[i][0];
-				int x = offsets[i][1];
-				neighborPixel = *(OGPixelAddress + (y * width + x));
-				redSum += (neighborPixel >> 16) & 0xFF;
-				greenSum += (neighborPixel >> 8) & 0xFF;
-				blueSum += (neighborPixel & 0xFF);
+                for (int x = -2; x <= 2; ++x) 
+                {
+                    if (!((y == -2 && x == -2) || 
+                        (y == 2 && x == -2) || 
+                        (y == -2 && x == 2) || 
+                        (y == 2 && x == 2))) 
+                    {
+
+                        int* neighborPixelAddress = currentPixelAddress + (y * width + x);
+
+                        // Extract color components
+                        int neighborPixel = *neighborPixelAddress;
+                        int red = (neighborPixel >> 16) & 0xFF;
+                        int green = (neighborPixel >> 8) & 0xFF;
+                        int blue = neighborPixel & 0xFF;
+
+                        // Accumulate values
+                        redSum += red;
+                        greenSum += green;
+                        blueSum += blue;
+                    }
+                }
             }
 
             // Compute averages
-            avgRed = (redSum / 21);
-            avgGreen = (greenSum / 21);
-            avgBlue = (blueSum / 21);
+            int avgRed = redSum / 21;
+            int avgGreen = greenSum / 21;
+            int avgBlue = blueSum / 21;
 
             // Update the current pixel
-            *updatedPixelAddress = (0xFF << 24) | (avgRed << 16) | (avgGreen << 8) | avgBlue;
+            *currentPixelAddress = (0xFF << 24) | (avgRed << 16) | (avgGreen << 8) | avgBlue;
         }
 
     }
--- Circus/Circus.h
+++ Circus/Circus.h
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <emmintrin.h>
 
 #ifdef CIRCUS_EXPORTS
 #define CIRCUS_API __declspec(dllexport)
@@ -10,4 +9,4 @@
 #define CIRCUS_API __declspec(dllimport)
 #endif
 
-extern "C" CIRCUS_API void CompressionFuncCircus(int*, int, int, const int*, int*);
+extern "C" CIRCUS_API void CompressionFuncCircus(int*, int, int, const int*);
